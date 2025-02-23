289295cd3e1ca43469ed3cfb3d8ed7cfcbe0151c
“horacenjoroge” <horacenjorge@gmail.com>
2025-02-23 14:55:24 +0300

    Express REST API with MongoDB and Mongoose Part 1

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
 const commentSchema = new Schema({
     rating: {
         type: Number,
@@ -21,18 +25,35 @@ const commentSchema = new Schema({
 });
 
 const campsiteSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    comments: [commentSchema]
+name: {
+    type: String,
+    required: true,
+    unique: true
+},
+description: {
+    type: String,
+    required: true
+},
+image: {
+    type: String,
+    required: true
+},
+elevation: {
+    type: Number,
+    required: true
+},
+cost: {
+    type: Currency,
+    required: true,
+    min: 0
+},
+featured: {
+    type: Boolean,
+    default: false
+},
+comments: [commentSchema]
 }, {
-    timestamps: true
+timestamps: true
 });
 
 const Campsite = mongoose.model('Campsite', campsiteSchema);
